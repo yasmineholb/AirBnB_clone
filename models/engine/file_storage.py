2145a56188c0656a0389e3ef99a166df865d18e8
yasmineholb <1055@holbertonschool.com>
2020-02-14 22:42:42 +0100

    class creation

---
+++ models/engine/file_storage.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""
+This module contains FileStorage class
+"""
+
+class FileStorage:
+    """This class serializes instances to a JSON file and deserializes JSON file to instances"""
