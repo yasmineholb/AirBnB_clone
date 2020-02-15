e61a6601c38f1107b5de3ca91077242567172f2f
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-02-15 23:27:59 +0100

    remove file.json, add some documentation

--- console.py
+++ console.py
@@ -1,33 +1,32 @@
 #!/usr/bin/python3
+""" entry point of the command interpreter """
 import cmd
-
-import sys
 
 
 class HBNBCommand(cmd.Cmd):
-    """ """
+    """ cmd class """
     prompt = '(hbnb) '
 
     def main():
+        """ main function """
         do_EOF(self, arg)
         do_quit(self, arg)
         emptyline(self)
 
     def do_quit(self, arg):
         """Quit command to exit the program
-
         """
         quit()
         return True
 
     def do_EOF(self, arg):
-        """EOF command to exit the program
-
+        """
+        EOF command to exit the program
         """
         return True
 
     def emptyline(self):
-        """ """
+        """ do nothing when an empty line is entered """
         pass
 
 if __name__ == '__main__':
