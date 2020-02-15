8274fee7e3440bb3d0dfef5009ccc91c67709b29
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-02-15 20:29:10 +0100

    TASK 5 DONE git add -Agit add -Agit add -Agit add -Agit add -Agit add -A!

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,19 +20,25 @@ class FileStorage:
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
         k = obj.__class__.__name__ + "." + str(obj.id)
-        FileStorage.__objects[k] = obj.to_dict()
+        FileStorage.__objects[k] = obj
 
     def save(self):
         """ serializes __objects to the JSON file """
         with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
-            j = json.dumps(FileStorage.__objects)
+            a = {}
+            for k, v in FileStorage.__objects.items():
+                a[k] = v.to_dict()
+            j = json.dumps(a)
             f.write(j)
 
     def reload(self):
         """ deserializes the JSON file to __objects """
         try:
             with open(FileStorage.__file_path, "r") as f:
-                data = f.read()
-                FileStorage.__objects = json.loads(data)
+                from models.base_model import BaseModel
+                data = json.load(f)
+                for v in data.values():
+                    obj = BaseModel(**v)
+                    self.new(obj)
         except:
             pass
