a2fc7ac94ec7499b07ad2b1b4c37599e946d5f91
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-02-17 23:05:16 +0100

    class.all() class.count() class.show(id) class.destroy(id)

--- console.py
+++ console.py
@@ -28,30 +28,81 @@ class HBNBCommand(cmd.Cmd):
         emptyline(self)
 
     def default(self, arg):
+        if '.' not in arg:
+            print("*** Unknown syntax: ", arg)
+            return
         y = arg.split('.')
-        if  y[0] in HBNBCommand.classes:
-            if y[1] == "all()":
-                objects = storage.all()
-                l = []
-                for v in objects.values():
-                    if v.__class__.__name__ == y[0]:
-                        l.append(str(v))
-                print(l)
-            elif y[1] == "count()":
-                objects = storage.all()
-                l = 0
-                for v in objects.values():
-                    if v.__class__.__name__ == y[0]:
-                        l += 1
-                print(l)
-            elif y[1][0:4] == "show":
-                m = y[1][4:].split('(')
-                d = m[1].split(')')
-                c = d[0]
-                objects = storage.all()
-                if c in objects:
-                    print(objects[c])
+        if y[0] == "":
+            print("** class name missing **")
+            return
+        if y[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if y[1] == "all()":
+            self.fn_all(y[0])
+        elif y[1] == "count()":
+            self.fn_count(y[0])
+        elif y[1][0:5] == "show(" and y[1][-1] == ")":
+            self.fn_show(y[0], y[1])
+        elif y[1][0:8] == "destroy(" and y[1][-1] == ")":
+            self.fn_destroy(y[0], y[1])
 
+    def fn_all(self, y0):
+        """
+        function <class>.all()
+        Prints all string representation of all instances
+        based or not on the class name
+        """
+        objects = storage.all()
+        print("[", end="")
+        c = 0
+        for v in objects.values():
+            if v.__class__.__name__ == y0:
+                if c != 0:
+                    print(", ", end="")
+                c += 1
+                print(v, end="")
+        print("]")
+
+    def fn_count(self, y0):
+        """ retrieve the number of instances of a class """
+        objects = storage.all()
+        c = 0
+        for v in objects.values():
+            if v.__class__.__name__ == y0:
+                c += 1
+        print(c)
+
+    def fn_show(self, y0, y1):
+        """ retrieve an instance based on its ID """
+        m = y1.split('(')
+        d = m[1].split(')')
+        my_id = d[0]
+        if my_id == "":
+            print("** instance id missing **")
+            return
+        objects = storage.all()
+        for v in objects.values():
+            if my_id == v.id and v.__class__.__name__ == y0:
+                print(v)
+                return
+        print("** no instance found **")
+
+    def fn_destroy(self, y0, y1):
+        """ destroys an instance based on his ID """
+        m = y1.split('(')
+        d = m[1].split(')')
+        my_id = d[0]
+        if my_id == "":
+            print("** instance id missing **")
+            return
+        key = y0 + "." + my_id
+        objects = storage.all()
+        if key in objects:
+            del objects[key]
+            storage.save()
+        else:
+            print("** no instance found **")
 
     def do_create(self, arg):
         """
