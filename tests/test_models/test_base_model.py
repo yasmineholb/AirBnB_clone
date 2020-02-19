b2754dc1c18df6cf7d349d820e41e363259e4a00
Mariem Matri <ilyesjihenmeriem@yahoo.fr>
2020-02-19 22:35:33 +0100

    unittests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -4,16 +4,23 @@ Unittest for BaseModel class
 """
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
 
 
 class testbase(unittest.TestCase):
     """
     unittests for BaseModel class
     """
-    def test_id(self):
+    def test_attributes(self):
         """
-        check id
+        check attributes
         """
         b1 = BaseModel()
         b2 = BaseModel()
         self.assertNotEqual(b1.id, b2.id)
+        b1.name = "Mariem"
+        self.assertEqual(b1.name, "Mariem")
+        self.assertTrue(isinstance(b1.created_at, datetime))
+        self.assertTrue(isinstance(b2.updated_at, datetime))
+        self.assertTrue(type(b1.id) is str)
+        self.assertTrue(type(b1.to_dict()), dict)
